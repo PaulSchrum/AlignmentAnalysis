23e7982275587dc68b5d4a1afe2fcbe587f605a6
Paul Schrum <ptschrum@ncsu.edu>
2018-06-10 20:35:04 -0600

    Added method call to CreateExtendedPointSequenceFromLatLong

--- MotionStudy.py
+++ MotionStudy.py
@@ -1,14 +1,18 @@
 import sys, os
 from gpsAnalyst import dynamicCsvRecord, createInstanceFromCsv
 import gpsAnalyst
+import ExtendedPointSequence as EPS
 
 
 testDir = os.path.join(os.getcwd(), 'testData')
 
 inputFile = os.path.join(testDir, r"20180606 Albuquerque.csv")
-newInstance = createInstanceFromCsv(inputFile)
+gpsPointSequence = createInstanceFromCsv(inputFile)
 
-outFile = os.path.join(testDir, '20180606 Albuquerque.kml')
-theKml = gpsAnalyst.saveAsKML(newInstance, outFile)
+# outFile = os.path.join(testDir, '20180606 Albuquerque.kml')
+# theKml = gpsAnalyst.saveAsKML(gpsPointSequence, outFile)
+
+ExtendedPointSequence = EPS.CreateExtendedPointSequenceFromLatLong(gpsPointSequence)
+maxSpeed = max([x.speed for x in ExtendedPointSequence])
 
 dbg = True
